09c8617d2ff53ea0e567965619ee5165f7c1f3dd
Daniel Dills <dills253@gmail.com>
2021-04-17 17:55:47 -0700

    installed superagent, updated code to include superagent functionality

--- server.js
+++ server.js
@@ -1,36 +1,43 @@
 'use strict';
 
-const express = require('express');
+const superagent = require('superagent');
 require('dotenv').config();
-const cors = require('cors');
+const express = require('express');
 
-const weatherData = require('./data/weather.json');
-
+// const cors = require('cors');
 const app = express();
-app.use(cors());
+
+// const weatherData = require('./data/weather.json');
+
+
+// app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
-app.get('/', (request, response) => {
-  response.send('hello!');
+console.log('Hello');
+
+app.get('/weather', (request, response) => {
+  superagent.get('https://api.weatherbit.io/v2.0/forecast/daily')
+  .query({
+    key: process.env.WEATHERBIT_API_KEY,
+    units: 'I',
+    lat: request.query.lat,
+    lon: request.query.lon
+  })
+  .then(weatherData => {
+    response.json(weatherData.body.data.map(info => (
+    {date: info.datetime,
+      description: info.weather.description})));
+  });
 });
 
-  app.get('/weather', (request, response) => {
-    try {
-    const dailyForecasts = weatherData.data.map(day => new Forecast(day));
-    response.send(dailyForecasts);
-  } catch (error) {
-    handleErrors(error, response);
-  }
-});
+// function Forecast(obj) {
+//   this.date = obj.datetime;
+//   this.description = obj.weather.description;
+// }
 
-function Forecast(obj) {
-  this.date = obj.datetime;
-  this.description = obj.weather.description;
-}
-
-function handleErrors (error, response) {
-  response.status(500).send('Internal Error');
-}
+// function handleErrors (error, response) {
+//   response.status(500).send('Internal Error');
+// }
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
